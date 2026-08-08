defa3eb3a2f22b8dedb144c0ba6989137c73f98d
Snehalkale24 <snehalbkale2004@gmail.com>
2026-08-08 23:28:46 +0530

    add max.java

---
+++ max.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class max{
+	public static void main(String args[])
+	{
+		ArrayList<Integer>list=new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(10);
+
+		int max=list.get(0);
+
+
+		for(int i=0;i<list.size();i++)
+		{
+		    if(list.get(i)>max)
+		    {
+		    	max=list.get(i);
+		    }
+		}
+		System.out.println("max:"+max);
+	}
+}
